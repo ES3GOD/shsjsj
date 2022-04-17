646ca14f40c4414b1cec8128e3a6ecebbf29bae7
✗ GօɖEʏɛ ✗ <75217387+GodEyeX@users.noreply.github.com>
2022-04-17 14:50:44 +0530

    Create __init__.py

---
+++ GodAfk/__init__.py
@@ -0,0 +1,26 @@
+from Conf import *
+import logging
+import os
+import sys
+import time
+import telegram.ext as tg
+from telethon import TelegramClient
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+ENV = bool(os.environ.get("ENV", False))
+BOT_TOKEN = Config.BOT_TOKEN
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+DB_URI = Config.SQLALCHEMY_DATABASE_URI
+LOAD = Config.LOAD
+NO_LOAD = Config.NO_LOAD
+WORKERS = Config.WORKERS
+
+updater = tg.Updater(BOT_TOKEN, workers=WORKERS, use_context=True)
+dispatcher = updater.dispatcher
