6d849345af444697cb674d8fca183343a06ae653
✗ GօɖEʏɛ ✗ <75217387+GodEyeX@users.noreply.github.com>
2022-04-17 14:48:04 +0530

    Create config.py

---
+++ Conf/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config(object):
+    LOGGER = True
+    LOAD = []
+    NO_LOAD = []
+    WORKERS = 8
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    API_ID = os.environ.get("API_ID", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+
+class PIPER(Config):
+    LOGGER = True
